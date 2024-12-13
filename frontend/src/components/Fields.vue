25f38a10f72b344840ae422944921d61d811b612
leandro <ljpcarvalho@gmail.com>
2024-12-12 21:16:20 -0300

    frontend - chore: clean up and set up initial project configuration
    - Remove unnecessary files generated by `create-vue`
    - Set up the initial project structure
    - Add basic development dependencies

---
+++ frontend/src/components/Fields.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginForm',
+})
+</script>
+
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form>
+      <div>
+        <label for="email">Email</label>
+        <input type="email" id="email" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" />
+      </div>
+      <button type='button'>Login</button>
+    </form>
+  </div>
+</template>
---
+++ frontend/src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/Login.view.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+    },
+  ],
+})
+
+export default router
