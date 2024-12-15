9aec6ff59d078a783f02360c499bf9bcafe41369
leandro <ljpcarvalho@gmail.com>
2024-12-15 11:32:15 -0300

    backend - test: middleware and route

--- __tests__/backend/middleware/index.test.ts
+++ __tests__/backend/middleware/index.test.ts
@@ -1,6 +1,11 @@
 import * as chai from "chai";
+import sinonChai from "sinon-chai";
+import e, { Request, Response, NextFunction } from "express";
 import Sinon from "sinon";
 import MiddleWare from "../../../backend/src/layers/middleware/index.middleware";
+import { ServiceHelpers } from "../../../backend/src/utils";
+
+chai.use(sinonChai);
 
 const { expect } = chai;
 
@@ -11,18 +16,160 @@ beforeEach(() => {
 const { protectedMiddleware, validationMiddleware } = MiddleWare;
 
 describe("protectedMiddelware", async function () {
-  const req = {} as Request;
-  req.headers.append("Authorization", "");
-  const res = {
-    send: Sinon.spy().returns(),
-  } as Response;
+  it("deve retornar 401 se token nao for fornecido", async function () {
+    const req = {
+      headers: {},
+    } as Request;
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
 
-  const next = Sinon.spy() as NextFunction;
+    const next = Sinon.stub() as unknown as NextFunction;
 
-  await protectedMiddleware()(req, res, next);
+    await protectedMiddleware()(req, res, next);
 
-  expect(res).has.been.calledWith({
-    status: 401,
-    body: { message: "Voce precisa estar logado" },
+    expect(res.status).has.been.calledOnceWith(401);
+    expect(res.json).has.been.calledOnceWith({
+      message: "Voce precisa estar logado",
+    });
+  });
+  it("deve retornar 401 se o token for invalido", async function () {
+    const req = {
+      headers: {
+        authorization: "Bearer invalidToken",
+      },
+    } as Request;
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
+
+    const next = Sinon.stub() as unknown as NextFunction;
+
+    await protectedMiddleware()(req, res, next);
+
+    expect(res.status).has.been.calledOnceWith(401);
+    expect(res.json).has.been.calledOnceWith({
+      message: "Token invalido",
+    });
+  });
+  it("deve chamar a funcao next", async function () {
+    const req = {
+      headers: {
+        authorization: ServiceHelpers.createToken({ id: 1 }),
+      },
+    } as Request;
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
+    const next = Sinon.stub() as unknown as NextFunction;
+    await protectedMiddleware()(req, res, next);
+    expect(next).has.been.calledOnce;
+  });
+});
+
+describe.only("validationMidlleware", async function () {
+  it("deve retornar 400 se o body nao for valido", async function () {
+    const req = {
+      body: {
+        email: "invalidEmail",
+      },
+    };
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
+    const next = Sinon.stub() as unknown as NextFunction;
+    await validationMiddleware("user", "userSchema")(req as Request, res, next);
+    expect(res.status).has.been.calledOnceWith(400);
+  });
+
+  it("deve chamar a funcao next", async function () {
+    const req = {
+      body: {
+        name: "teste",
+        email: "a@a.com",
+        password: "123456",
+      },
+    };
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
+    const next = Sinon.stub() as unknown as NextFunction;
+    await validationMiddleware("user", "userSchema")(req as Request, res, next);
+    expect(next).has.been.calledOnce;
+  });
+
+  it("na no teste login deve retornar 400, caso nao tenha email ou password", async function () {
+    const req = {
+      body: {},
+    };
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
+    const next = Sinon.stub() as unknown as NextFunction;
+    await validationMiddleware("user", "loginSchema")(
+      req as Request,
+      res,
+      next
+    );
+
+    expect(res.status).has.been.calledWith(400);
+
+    const req2 = {
+      body: {
+        email: "a@a.com",
+      },
+    };
+    await validationMiddleware("user", "loginSchema")(
+      req2 as Request,
+      res,
+      next
+    );
+    expect(res.status).has.been.calledWith(400);
+  });
+
+  it("na no teste login deve retornar 400, caso nao tenha email ou password", async function () {
+    const req = {
+      body: {
+        email: "a",
+      },
+    };
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
+    const next = Sinon.stub() as unknown as NextFunction;
+    await validationMiddleware("user", "loginSchema")(
+      req as Request,
+      res,
+      next
+    );
+
+    expect(res.status).has.been.calledWith(400);
+  });
+
+  it("na no teste login deve chamar o next", async function () {
+    const req = {
+      body: {
+        email: "a@a.com",
+        password: "123456",
+      },
+    };
+    const res = {
+      json: Sinon.stub().returnsThis(),
+      status: Sinon.stub().returnsThis(),
+    } as unknown as Response;
+    const next = Sinon.stub() as unknown as NextFunction;
+    await validationMiddleware("user", "loginSchema")(
+      req as Request,
+      res,
+      next
+    );
+    expect(next).has.been.calledOnce;
   });
 });
--- __tests__/backend/routes/index.test.ts
+++ __tests__/backend/routes/index.test.ts
@@ -1,23 +1,15 @@
-import {use, expect} from "chai";
+import * as chai from "chai";
 import chaiHttp from "chai-http";
-import Sinon from "sinon";
 import server from "../../../backend/src/server";
 import { Routes } from "../../../shared/mapping/route";
 
-use(chaiHttp);
+// chai.use(chaiHttp);
 
-beforeEach(() => {
-  Sinon.restore();
-});
-
-describe("controller", async function () {
-  const app = request.execute(server);
-  it("profile", async function () {
-    const req = {};
-    const res = {
-      send: Sinon.spy(),
-    };
-    const response = app.get(`/api${Routes.getRoute("profile")}`);
-    console.log(response);
-  });
-});
+// describe("controller", async function () {
+//   it.only("profile", async function () {
+//     const response = await chai
+//       .request(server)
+//       .get(`/api${Routes.getRoute("profile")}`);
+//     console.log(response);
+//   });
+// });
